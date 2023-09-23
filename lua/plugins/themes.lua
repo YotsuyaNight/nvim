2a8642ef6958520f263c09ec6ab1f17105d9f358
Yotsuya <yotsuya.dreams@gmail.com>
2023-09-23 22:10:33 +0200

    Add nightfly color config

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "nightfly",
+  --   },
+  -- },
+}
